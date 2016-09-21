10d2da5b83a44a47d4245322c5412bc1dc272978
Frank Broniewski <frank.broniewski@gmail.com>
2016-09-21 09:05:13 +0200

    project initialization
    A Scroller to scroll through the features of a GeoJson object and return
    the properties of the feature on scroll

---
+++ GeoJsonScroller.js
@@ -0,0 +1,12 @@
+/**
+ * A Scroller to scroll through the features of a GeoJson object
+ * and return the properties of the feature on scroll
+ *
+ * geojson: the geosjon object to scroll through
+ * properties: which properties to return
+ * sortfunc: an optional sort function for sorting the features
+ * geometry: 'yes' or 'no', return the geometry of the feature with the properties
+ */
+var GeoJsonScroller = function (geojson, properties, sortfunc, geometry) {
+
+}
